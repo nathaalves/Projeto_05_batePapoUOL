35c36274a2d77aef2e41ac87673af015405b237d
nathaalves <nathaalvesaraujo@gmail.com>
2022-04-13 14:12:06 -0300

    Chat: requisição das mensagens

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
+
+let userName = ""
+
 function enterTheRoom() {
-    const userName = document.querySelector(".user-name").value
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: userName})
-    promise.then(successConection)
+    userName = document.querySelector(".user-name").value
+    const conection = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: userName})
+    
+    conection.then(function () {
+        setInterval(keepConnection, 4000)
+        requestMessages()
+    })
+    
 }
 
 function renderMessenges() {
 
 }
 
-function successConection() {
-    alert("Conectado")
+function requestMessages() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    console.log(promise)
+    promise.then(function (response) {
+        console.log(response.data)
+    })
+    promise.catch(function (error) {
+        console.log(error.response.status)
+        console.log(error.response.data)
+    })
 }
+
+function keepConnection() {
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {name: userName})
+    promise.then(function () {
+        console.log("conectado")
+    })
+}
+
+
